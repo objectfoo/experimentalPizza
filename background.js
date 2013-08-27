94180648dc9014550f8c96d4ff9d7792281aded2
Satake <satake@objectfoo.com>
2013-08-26 21:26:22 -0700

    persist body.pizza across reloads, rename scripts

---
+++ background.js
@@ -0,0 +1,47 @@
+/*global chrome*/
+(function () {
+    'use strict';
+
+    var active = {};
+
+    function existy(val) {
+        /*jshint eqnull: true*/
+        return val != null;
+    }
+
+    function truthy(val) {
+        return val !== false && existy(val);
+    }
+
+    function tabHasPizza(tabId) {
+        return truthy(active[tabId]);
+    }
+
+    function addPizza(tabId) {
+        active[tabId] = true;
+        chrome.tabs.executeScript({code: 'document.body.classList.add("pizza")'});
+        chrome.browserAction.setIcon({tabId: tabId, path: "images/pizzaOn.png"});
+    }
+
+    function removePizza(tabId) {
+        active[tabId] = false;
+        chrome.tabs.executeScript({code: 'document.body.classList.remove("pizza")'});
+        chrome.browserAction.setIcon({tabId: tabId, path: "images/pizzaOff.png"});
+    }
+
+    function togglePizza(tab) {
+        if (tabHasPizza(tab.id))
+            removePizza(tab.id);
+        else
+            addPizza(tab.id);
+    }
+
+    chrome.browserAction.onClicked.addListener(togglePizza);
+
+    chrome.runtime.onMessage.addListener(function(msg, sender) {
+
+        if (msg.iCanHazPizza && tabHasPizza(sender.tab.id)) {
+            addPizza(sender.tab.id);
+        }
+    });
+})();
---
+++ content.js
@@ -0,0 +1,4 @@
+(function () {
+    'use strict';
+    chrome.extension.sendMessage({"iCanHazPizza": true});
+})();
